5a4a9cf9fe2f1f79740fb08ef22e227ea20ffa50
Naifa A. Syamil <240705153@student.ar-raniry.ac.id>
2025-11-19 19:12:15 +0700

    Add files via upload

---
+++ pertemuan 2/linkedlist.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self, data=None, pointer=None):
+        self.data = data
+        self.next = pointer
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_first(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def insert_at_last(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            node_sekarang = self.head
+            while node_sekarang.next:
+                node_sekarang = node_sekarang.next
+            
+            node = Node(data)
+            node_sekarang.next = node
+
+    def insert_at(self, index, data):
+        if index < 0 or index > self.length() - 1:
+            print("indeks tidak valid")
+        elif index == 0:
+            self.insert_at_first(data)
+        else:
+            urutan = 0
+            node_sekarang = self.head
+
+            while urutan < index - 1:
+                urutan += 1
+                node_sekarang = node_sekarang.next
+
+            node = Node(data, node_sekarang.next)
+            node_sekarang.next = node
+
+    def print(self):
+        if self.head is None:
+            print("data kosong")
+        else:
+            text_print = ''
+            node_sekarang = self.head
+
+            while node_sekarang:
+                text_print += str(node_sekarang.data) + "→"
+                node_sekarang = node_sekarang.next
+
+            print(text_print)
+
+    def length(self):
+        urutan = 0
+        data_sekarang = self.head
+
+        while data_sekarang:
+            data_sekarang = data_sekarang.next
+            urutan += 1
+
+        return urutan
+
+ll = LinkedList()
+#insert
+ll.insert_at_first("jeruk")
+ll.insert_at_first("mangga")
+ll.insert_at_first("manggis")
+ll.insert_at_last("apel")
+ll.insert_at(2, "durian")
+
+#print
+ll.print()
+print(ll.length())
